47a1db1d7d29f2a1ac09c1af7d70ed9b4ebafc29
Gurpinder <grpndrskang@gmail.com>
2018-02-15 23:47:21 +0530

    Fix "create and consume events" example.

--- examples/create-and-consume-events/main.go
+++ examples/create-and-consume-events/main.go
@@ -1,61 +1,20 @@
 // some updates for https://rsmitty.github.io/Kubernetes-Events/
 // and http://blog.ctaggart.com/2016/09/accessing-kubernetes-api-on-google.html
-
 package main
 
 import (
-	"encoding/base64"
 	"fmt"
 	"log"
 	"net/http"
 	"time"
 
-	"golang.org/x/net/context"
-	"google.golang.org/api/compute/v1"
-	"google.golang.org/api/container/v1"
-	// "google.golang.org/api/option"
-	// "google.golang.org/api/transport"
-	// "k8s.io/kubernetes/pkg/api"
-	// "k8s.io/kubernetes/pkg/client/cache"
-	// "k8s.io/kubernetes/pkg/client/restclient"
-	// "k8s.io/kubernetes/pkg/controller/informers"
-	// "k8s.io/kubernetes/pkg/util/wait"
-
 	api        "k8s.io/api/core/v1"
 	cache      "k8s.io/client-go/tools/cache"
-	clientset  "k8s.io/kubernetes/pkg/client/clientset_generated/internalclientset"
+	clientset  "k8s.io/client-go/kubernetes"
 	informers  "k8s.io/client-go/informers"
 	restclient "k8s.io/client-go/rest"
-	v1beta1 "k8s.io/client-go/kubernetes/typed/admissionregistration/v1beta1"
 	wait       "k8s.io/apimachinery/pkg/util/wait"
 )
-
-func newKubernetesClient(clstr *container.Cluster) (*clientset.Clientset, error) {
-	cert, err := base64.StdEncoding.DecodeString(clstr.MasterAuth.ClientCertificate)
-	if err != nil {
-		return nil, err
-	}
-	key, err := base64.StdEncoding.DecodeString(clstr.MasterAuth.ClientKey)
-	if err != nil {
-		return nil, err
-	}
-	ca, err := base64.StdEncoding.DecodeString(clstr.MasterAuth.ClusterCaCertificate)
-	if err != nil {
-		return nil, err
-	}
-	config := &restclient.Config{
-		Host:            clstr.Endpoint,
-		TLSClientConfig: restclient.TLSClientConfig{CertData: cert, KeyData: key, CAData: ca},
-		Username:        clstr.MasterAuth.Username,
-		Password:        clstr.MasterAuth.Password,
-		// Insecure:        true,
-	}
-	kbc, err := clientset.NewForConfig(config)
-	if err != nil {
-		return nil, err
-	}
-	return kbc, nil
-}
 
 func podCreated(obj interface{}) {
 	pod := obj.(*api.Pod)
@@ -70,11 +29,33 @@ func podDeleted(obj interface{}) {
 func watchPods(kbc *clientset.Clientset) {
 	resyncPeriod := 30 * time.Minute
 	si := informers.NewSharedInformerFactory(kbc, resyncPeriod)
-	si.Pods().Informer().AddEventHandler(
+	si.Core().V1().Pods().Informer().AddEventHandler(
 		cache.ResourceEventHandlerFuncs{
 			AddFunc:    podCreated,
 			DeleteFunc: podDeleted,
 		},
 	)
 	si.Start(wait.NeverStop)
+}
+
+func main() {
+
+	//Configure cluster info
+	config := &restclient.Config{
+		Host:     "https://xxx.yyy.zzz:443",
+		Username: "kube",
+		Password: "supersecretpw",
+	}
+
+	//Create a new client to interact with cluster and freak if it doesn't work
+	kbc, err := clientset.NewForConfig(config)
+	if err != nil {
+		log.Fatalln("Client not created sucessfully:", err)
+	}
+
+	//Watch for Pods
+	watchPods(kbc)
+
+	//Keep alive
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
